ba17daa6ed6785f94bec6e15d4b0934ce650e921
Robert Varga <varga.robert@gmail.com>
2014-01-18 13:51:57 +0100

    commit 2

---
+++ Tests/ListBoxExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Misc;
+
+namespace Tests
+{
+	[TestClass]
+	public class ListBoxExtensions
+	{
+		[TestMethod]
+		public void TextArray()
+		{
+			var lb = new ListBox();
+			lb.Items.Add("abc");
+			lb.Items.Add("def");
+			var arr = lb.GetTextsArray();
+
+			Assert.AreEqual(arr.Length, 2);
+		}
+	}
+}
